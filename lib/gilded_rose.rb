95aacdd281ab66737dae5035ef5df5be893bf553
Dom Tunstill <datunstill@gmail.com>
2019-11-21 21:56:36 +0000

    removed testing dependencies and cleared rubocop errors. ALl passing tests and rubocop checks

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,29 +4,30 @@ require_relative 'item'
 
 # GildedRose class is the default class and handles the list of items
 class GildedRose
-  SPECIAL_ITEMS = {
-    'Sulfuras, Hand of Ragnaros' => LegendaryItem,
-    'Aged Brie' => CheeseItem,
-    'Backstage passes to a TAFKAL80ETC concert' => BackstagePassItem,
-    'Conjured Mana Cake' => ConjuredItem
-  }.freeze
-
-  def initialize(items)
+  def initialize(items, **item_classes)
     @items = items
-    sort_items
-  end
+    @normal_item = item_classes[:normal_item] || NormalItem
+    @special_items = {
+      'Sulfuras, Hand of Ragnaros' => item_classes[:legendary_item] || LegendaryItem,
+      'Aged Brie' => item_classes[:cheese_item] || CheeseItem,
+      'Backstage passes to a TAFKAL80ETC concert' => item_classes[:backstage_pass_item] || BackstagePassItem,
+      'Conjured Mana Cake' => item_classes[:conjured_item] || ConjuredItem
+    }
 
-  def update_quality
-    @items.each(&:update_quality)
+    sort_items
   end
 
   def sort_items
     @items.map! do |item|
-      if SPECIAL_ITEMS.include?(item.name)
-        SPECIAL_ITEMS[item.name].new(item.name, item.sell_in, item.quality)
+      if @special_items.include?(item.name)
+        @special_items[item.name].new(item.name, item.sell_in, item.quality)
       else
-        NormalItem.new(item.name, item.sell_in, item.quality)
+        @normal_item.new(item.name, item.sell_in, item.quality)
       end
     end
+  end
+
+  def update_quality
+    @items.each(&:update_quality)
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,185 +3,135 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  describe 'hash of SPECIAL_ITEMS in GildedRose class' do
-    it 'has Sulfuras, Hand of Ragnaros stored' do
-      expect(GildedRose::SPECIAL_ITEMS).to include('Sulfuras, Hand of Ragnaros')
-    end
-    it 'has Backstage passes to a TAFKAL80ETC concert stored' do
-      expect(GildedRose::SPECIAL_ITEMS).to include(
-        'Backstage passes to a TAFKAL80ETC concert'
-      )
-    end
-    it 'has Aged Brie stored' do
-      expect(GildedRose::SPECIAL_ITEMS).to include('Aged Brie')
-    end
-    it 'has Conjured Mana Cake has stored' do
-      expect(GildedRose::SPECIAL_ITEMS).to include('Conjured Mana Cake')
-    end
+  let(:normal_item) do
+    double :normal_item,
+           name: 'Elixir of the Mongoose',
+           sell_in: 0,
+           quality: 0
+  end
+  let(:conjured_item) do
+    double :conjured_item,
+           name: 'Conjured Mana Cake',
+           sell_in: 0,
+           quality: 0
+  end
+  let(:cheese_item) do
+    double :cheese_item,
+           name: 'Aged Brie',
+           sell_in: 0,
+           quality: 0
+  end
+  let(:legendary_item) do
+    double :legendary_item,
+           name: 'Sulfuras, Hand of Ragnaros',
+           sell_in: 0,
+           quality: 0
+  end
+  let(:backstage_pass_item) do
+    double :backstage_pass_item,
+           name: 'Backstage passes to a TAFKAL80ETC concert',
+           sell_in: 0,
+           quality: 0
+  end
+
+  let(:normal_item_class) { double :normal_item_class, new: normal_item }
+  let(:conjured_item_class) { double :conjured_class, new: conjured_item }
+  let(:cheese_item_class) { double :cheese_class, new: cheese_item }
+  let(:legendary_item_class) { double :legendary_class, new: legendary_item }
+  let(:backstage_pass_item_class) { double :backstage_pass_class, new: backstage_pass_item }
+
+  let(:items) do
+    [
+      normal_item,
+      conjured_item,
+      cheese_item,
+      legendary_item,
+      backstage_pass_item
+    ]
+  end
+
+  let(:item_classes) do
+    {
+      normal_item: normal_item_class,
+      conjured_item: conjured_item_class,
+      cheese_item: cheese_item_class,
+      legendary_item: legendary_item_class,
+      backstage_pass_item: backstage_pass_item_class
+    }
+  end
+
+  let(:gilded_rose) do
+    described_class.new(
+      items,
+      item_classes
+    )
   end
 
   describe '#sort_items' do
-    it 'returns an array of sorted normal item objects' do
-      items = [Item.new('Elixir of the Mongoose', 0, 0)]
-      list = GildedRose.new(items)
-      list.sort_items
-      expect(items[0].class).to eq NormalItem
+    it 'creates a new normal item if normal item is in the array' do
+      expect(normal_item_class).to receive(:new)
+      gilded_rose.sort_items
     end
 
-    it 'returns an array of sorted normal item objects' do
-      items = [Item.new('+5 Dexterity Vest', 0, 0)]
-      list = GildedRose.new(items)
-      list.sort_items
-      expect(items[0].class).to eq NormalItem
+    it 'creates a new legendary item if special item is in the array' do
+      expect(legendary_item_class).to receive(:new)
+      gilded_rose.sort_items
     end
 
-    it 'returns an array of sorted legendary item objects' do
-      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 80)]
-      list = GildedRose.new(items)
-      list.sort_items
-      expect(items[0].class).to eq LegendaryItem
+    it 'creates a new cheese item if special item is in the array' do
+      expect(cheese_item_class).to receive(:new)
+      gilded_rose.sort_items
     end
 
-    it 'returns an array of sorted cheese item objects' do
-      items = [Item.new('Aged Brie', 0, 12)]
-      list = GildedRose.new(items)
-      list.sort_items
-      expect(items[0].class).to eq CheeseItem
+    it 'creates a new backstage item if special item is in the array' do
+      expect(backstage_pass_item_class).to receive(:new)
+      gilded_rose.sort_items
     end
 
-    it 'returns an array of sorted backstage pass item objects' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 12)]
-      list = GildedRose.new(items)
-      list.sort_items
-      expect(items[0].class).to eq BackstagePassItem
-    end
-
-    it 'returns an array of sorted conjured item objects' do
-      items = [Item.new('Conjured Mana Cake', 10, 12)]
-      list = GildedRose.new(items)
-      list.sort_items
-      expect(items[0].class).to eq ConjuredItem
+    it 'creates a new conjured stage item if special item is in the array' do
+      expect(conjured_item_class).to receive(:new)
+      gilded_rose.sort_items
     end
   end
 
   describe '#update_quality' do
+    before :each do
+      allow(normal_item).to receive(:update_quality)
+      allow(cheese_item).to receive(:update_quality)
+      allow(backstage_pass_item).to receive(:update_quality)
+      allow(legendary_item).to receive(:update_quality)
+      allow(conjured_item).to receive(:update_quality)
+    end
+
     it 'does not change the name' do
-      items = [Item.new('Elixir of the Mongoose', 0, 0)]
-      GildedRose.new(items).update_quality
+      allow(gilded_rose).to receive(:update_quality)
+      gilded_rose.update_quality
       expect(items[0].name).to eq 'Elixir of the Mongoose'
     end
 
-    it 'All items: quality of item cannot go negative' do
-      items = [Item.new('Elixir of the Mongoose', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
+    it 'calls updated_quality method on a normal item' do
+      expect(normal_item).to receive(:update_quality)
+      gilded_rose.update_quality
     end
 
-    it 'Sulfuras: never decrease/increase in quality' do
-      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 80)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 80
+    it 'calls updated_quality method on a legendary item' do
+      expect(legendary_item).to receive(:update_quality)
+      gilded_rose.update_quality
     end
 
-    it 'Sulfuras: never decrease/increase sellIn date' do
-      items = [Item.new('Sulfuras, Hand of Ragnaros', 0, 80)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq 0
+    it 'calls updated_quality method on a cheese item' do
+      expect(cheese_item).to receive(:update_quality)
+      gilded_rose.update_quality
     end
 
-    it 'Brie: items quality never more than 50' do
-      items = [Item.new('Aged Brie', 20, 50)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 50
+    it 'calls updated_quality method on a backstage pass item' do
+      expect(backstage_pass_item).to receive(:update_quality)
+      gilded_rose.update_quality
     end
 
-    it 'Backstage: items quality never more than 50' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 20, 50)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 50
-    end
-
-    it 'Normal: item decreases quality each day' do
-      items = [Item.new('Elixir of the Mongoose', 10, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 9
-    end
-
-    it 'Normal: item decreases sell_in each day' do
-      items = [Item.new('Elixir of the Mongoose', 10, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq 9
-    end
-
-    it 'Normal: quality twice decreases as fast after sell_in' do
-      items = [Item.new('Elixir of the Mongoose', 0, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 8
-    end
-
-    it 'Brie: items quality increases in value as it gets older' do
-      items = [Item.new('Aged Brie', 5, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 11
-    end
-
-    it 'Brie: quality increases twice as fast after sellin date' do
-      items = [Item.new('Aged Brie', 0, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 12
-    end
-
-    it 'Brie: sell_in value descreases by 1 each day' do
-      items = [Item.new('Aged Brie', 10, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq 9
-    end
-
-    it 'Backstage: quality increases each day' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 20, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 11
-    end
-
-    it 'Backstage: quality increases 2x if less than 10 days' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 9, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 12
-    end
-
-    it 'Backstage: quality increases 3x if less than 5 days' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 4, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 13
-    end
-
-    it 'Backstage: Quality drops to 0 after the concert' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 50)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
-    end
-
-    it 'Backstage: sell_in value descreases by 1 each day' do
-      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].sell_in).to eq 9
-    end
-
-    it 'Conjured: quality of item cannot go negative' do
-      items = [Item.new('Conjured Mana Cake', 0, 0)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 0
-    end
-    it 'Conjured: item decreases quality by 2 each day' do
-      items = [Item.new('Conjured Mana Cake', 10, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 8
-    end
-
-    it 'Conjured: quality twice decreases as fast after sell_in' do
-      items = [Item.new('Conjured Mana Cake', 0, 10)]
-      GildedRose.new(items).update_quality
-      expect(items[0].quality).to eq 6
+    it 'calls updated_quality method on a conjured item' do
+      expect(conjured_item).to receive(:update_quality)
+      gilded_rose.update_quality
     end
   end
 end
